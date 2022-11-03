32218d8b58aaf1302c4863ece435367f20634caf
brayhan12 <brayhan0503hoyos@gmail.com>
2022-11-03 11:22:39 -0500

    modificacion

---
+++ js/menu_sub_iten.js
@@ -0,0 +1,9 @@
+let botonmenujs =document.getElementById("botonmenu");
+
+botonmenujs.addEventListener("click",function() {
+    alert("hola mundo.")
+})
+let botomenjs =document.getElementById("botomen");
+botomenjs.addEventListener("click",function() {
+    alert("hola.")
+}) 
